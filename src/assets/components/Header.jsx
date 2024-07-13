34bfaa4d93a900411bc1ed1610dd1b9ccc0538ff
saverioannpy <saverio.annunziata@outlook.com>
2024-07-13 19:58:48 +0200

    vercel si triggera?

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,10 +3,9 @@ import logo from '../img/power.svg'
 const Header = () => {
     return(
         <>
-            <div className="header-container bg-[#213847] p-2 flex flex-col items-center">
+            <div className="header-container p-2 flex flex-col items-center bg-trasparent">
                 <a href="/">
                     <img className="w-[200px]" src={logo} alt="power ranger logo"/>
-                    <p>mammt</p>
                 </a>
             </div>
         </>
